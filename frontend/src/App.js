0cec6841a3deb5295ad5604254ea1ac7ffa36ae5
Joemar <joemartolero987@gmail.com>
2025-08-06 23:52:55 +0800

    First commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Navba } from './components/nav.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Navba />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/nav.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function Navba(){
+    return "This is navbar";
+}
+
+// export default nav;
