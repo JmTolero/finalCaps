e96f65a4cd197d20c939fe59a0214339e769c8c8
Joemar <joemartolero987@gmail.com>
2025-08-13 23:45:19 +0800

    modifying the register form and installing backend folder depedencies

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@
     import './App.css';
     import { LandingPage } from './pages/landingpage';
     import { Login } from "./pages/login.jsx";
+    import { UserRegister } from "./pages/userRegister.jsx";
     // import Nav from '../src/components/nav';
 
     function App() {
@@ -11,6 +12,7 @@
         <Routes>
           <Route path="/" element={<LandingPage />} />        
           <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<UserRegister />} />
         </Routes>
       </Router>
     </>
